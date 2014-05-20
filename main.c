523481efea081c177fc6a190a551cd984a79d75a
Eric Faehnrich <eric.faehnrich@paragon-inc.com>
2014-05-20 14:11:08 -0400

    main now has read-eval-print loop

--- main.c
+++ main.c
@@ -1,9 +1,47 @@
 #include <stdio.h>
+#include "token.h"
+#include "parse.h"
+#include "eval.h"
 
-int main(void){
-	/*while not eof, but at command line do we get eof*/
-	printf("test\n");
-	return 0;
+char line[100];
+
+void printToken(Token* token){
+  if(token!=NULL){
+	while(token!=NULL){
+      printf("%s", token->p);
+      token=token->next;
+      if(token!=NULL)
+        printf("->");
+    }
+    printf("->");
+  }
+  printf("NULL\n");
 }
 
+void printSexpr(Sexpr* sexpr){
+  if(sexpr == NULL){
+    printf(" NIL ");
+    return;
+  }
+  if(sexpr->atom!=NULL)
+    printf(" %s ", sexpr->atom);
+  else{
+    printf("(");
+    printSexpr(sexpr->inner->next);/*next to skip linked list head*/
+    printf(")");
+  }
+  printf("->");
+  printSexpr(sexpr->next);
+}
 
+int main(void){
+  printf(">");
+  while(gets(line)){
+    printf(line);
+    tokenizeStr(line);
+    printToken(getHeadToken()->next);
+    printSexpr(eval(parse(&getHeadToken()->next)));
+    printf("\n>");
+  }
+  return 0;
+}
